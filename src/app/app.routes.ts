7add68898b405855db02a0867c507e4de12ecac7
Leonaldo Pasaribu <leonaldo.pasaribu@amarbank.co.id>
2024-07-08 17:10:47 +0700

    feat: setup app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('src/app/presentations/dashboard/routes/dashboard.routes').then(
+        m => m.routes,
+      ),
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'dashboard' },
+];
