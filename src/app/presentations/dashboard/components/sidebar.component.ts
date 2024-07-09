4032dc24c957c1801c7c834d2bdbaeda4a25e59e
Leonaldo Pasaribu <leonaldo.pasaribu@amarbank.co.id>
2024-07-09 08:57:32 +0700

    refactor: adjust router link with value from route url constant

--- src/app/presentations/dashboard/components/sidebar.component.ts
+++ src/app/presentations/dashboard/components/sidebar.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import {
+  DASHBOARD_ABOUT_ROUTE_URL,
+  DASHBOARD_LANDING_ROUTE_URL,
+} from 'src/app/shared/constants/route-url.constant';
+
 @Component({
   standalone: true,
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   imports: [RouterModule],
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  readonly aboutMenuRouteUrl: typeof DASHBOARD_ABOUT_ROUTE_URL;
+  readonly dashboardMenuRouteUrl: typeof DASHBOARD_LANDING_ROUTE_URL;
+
+  constructor() {
+    this.aboutMenuRouteUrl = DASHBOARD_ABOUT_ROUTE_URL;
+    this.dashboardMenuRouteUrl = DASHBOARD_LANDING_ROUTE_URL;
+  }
+}
