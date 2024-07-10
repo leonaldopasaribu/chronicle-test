cfd141bb5c684f12ed713898584f8c852323b6a9
Leonaldo Pasaribu <leonaldo.pasaribu@amarbank.co.id>
2024-07-10 09:30:49 +0700

    feat: setup loading screen in landing component

--- src/app/presentations/dashboard/containers/landing.component.ts
+++ src/app/presentations/dashboard/containers/landing.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  Observable,
+  Subscription,
+  SubscriptionLike,
+  distinctUntilChanged,
+} from 'rxjs';
 
 import { LandingStore } from '../stores/landing.store';
 import { LandingViewModel } from '../view-models/landing.view-model';
@@ -9,10 +15,13 @@ import { PlotRepositoryChronicle } from 'src/app/data/plot/plot.repository.chron
 import { PlotGeometryMapperChronicle } from 'src/app/data/plot/plot-geometry.mapper.chronicle';
 import { PlotPropertiesMapperChronicle } from 'src/app/data/plot/plot-properties.mapper.chronicle';
 
+import { DynamicComponentLoaderService } from 'src/app/shared/services/dynamic-component-loader';
+
 @Component({
   standalone: true,
   templateUrl: './landing.component.html',
   providers: [
+    DynamicComponentLoaderService,
     LandingStore,
     LandingViewModel,
     PlotMapperChronicle,
@@ -21,10 +30,36 @@ import { PlotPropertiesMapperChronicle } from 'src/app/data/plot/plot-properties
     { provide: PlotRepository, useClass: PlotRepositoryChronicle },
   ],
 })
-export class LandingComponent implements OnInit {
-  constructor(private landingViewModel: LandingViewModel) {}
+export class LandingComponent implements OnInit, OnDestroy {
+  isLoading$: Observable<boolean>;
+
+  private subscription: Subscription;
+
+  constructor(private landingViewModel: LandingViewModel) {
+    this.isLoading$ = this.landingViewModel.isLoading$;
+
+    this.subscription = new Subscription();
+  }
 
   ngOnInit(): void {
+    this.subscription.add(this.subscribeToIsLoading());
     this.landingViewModel.initializeMap();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  private subscribeToIsLoading(): SubscriptionLike {
+    return this.isLoading$
+      .pipe(distinctUntilChanged())
+      .subscribe((isLoading: boolean) => {
+        if (isLoading) {
+          this.landingViewModel.showLoading();
+          return;
+        }
+
+        this.landingViewModel.hideLoading();
+      });
   }
 }
--- src/app/presentations/dashboard/view-models/landing.view-model.ts
+++ src/app/presentations/dashboard/view-models/landing.view-model.ts
@@ -1,6 +1,8 @@
+import { DialogRef } from '@angular/cdk/dialog';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import leaflet from 'leaflet';
+import { distinctUntilChanged, map, Observable } from 'rxjs';
 
 import {
   DEFAULT_LATITUDE,
@@ -8,22 +10,51 @@ import {
   DEFAULT_ZOOM_LEVEL,
   MAP_BOUNDARIES,
 } from '../constants/landing.constant';
+import { LandingState } from '../states/landing.state';
 import { LandingStore } from '../stores/landing.store';
 
 import { PlotEntity } from 'src/app/core/entities/plot.entity';
 import { PlotPropertiesEntity } from 'src/app/core/entities/plot-properties.entity';
 import { PlotRepository } from 'src/app/core/repositories/plot.repository';
 
+import { LoadingScreenComponent } from 'src/app/shared/components/loading-screen';
+import { DynamicComponentLoaderService } from 'src/app/shared/services/dynamic-component-loader';
+
 @Injectable()
 export class LandingViewModel {
+  loadingDialogRef: DialogRef<unknown, LoadingScreenComponent> | undefined;
   map!: leaflet.Map;
   selectedPlot: PlotEntity | null;
 
   constructor(
+    private dynamicComponentLoaderService: DynamicComponentLoaderService,
     private landingStore: LandingStore,
     private plotRepository: PlotRepository,
   ) {
     this.selectedPlot = null;
+  }
+
+  get isLoading$(): Observable<boolean> {
+    return this.landingStore.state$.pipe(
+      map(({ isLoading }: LandingState) => isLoading),
+      distinctUntilChanged(),
+    );
+  }
+
+  showLoading(): void {
+    this.loadingDialogRef = this.dynamicComponentLoaderService.load(
+      LoadingScreenComponent,
+      {
+        loadStrategy: 'dialog',
+        positionStrategy: ['centerVertically', 'centerHorizontally'],
+      },
+    );
+  }
+
+  hideLoading(): void {
+    if (this.loadingDialogRef) {
+      this.loadingDialogRef.close();
+    }
   }
 
   initializeMap(): void {
